5b7b55bd19ea9f7154c4d1afdccca6f0e97a44f3
fasahath <syedafasahath@gmail.com>
2020-11-16 18:09:36 -0500

    adding level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -13,124 +13,131 @@ data.forEach((ufosighting) => {
 
 var button=d3.select("#filter-btn");
 var form=d3.select("#form");
-var date=d3.select("#datetime")
-var stchange=d3.select("#state");
+var dateEvnt=d3.select("#datetime");
+var countryEvnt=d3.select("#country");
+var stateEvnt=d3.select("#state");
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
-date.on("change",runDate);
-stchange.on("change",runState);
+dateEvnt.on("change",runDate);
+countryEvnt.on("change",runCountry);
+//stateEvnt.on("change",runState);
 
-
-// Complete the event handler function for the form
-function runEnter() {
-
-    // // Prevent the page from refreshing
-    // d3.event.preventDefault();
-    
-    // // Select the input element and get the raw HTML node
-    // var inputElement = d3.select("#datetime");
-  
-    // // Get the value property of the input element
-    // var inputValue = inputElement.property("value");
-  
-    // console.log(inputValue);
-  
-    // var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-    // var dupstates=filteredData.map(entry=>entry.state);
-    // var ust=[...new Set(dupstates)];
-    // console.log(ust[2]);
-    // // var uniquestate=[];
-    // // for (var i=0; i < dupstates.length; i++){
-    // //   if(dupstates[i]) not in (uniquestate)
-    // //     uniquestate.push(dupstates[i]);
-      
-    // // }
-    // // console.log(uniquestate);
-    // console.log(filteredData);
-    // var state=d3.select("#state");
-    // for (var i=0;i<ust.length; i++){
-    //   //var selstate=d3.select("#state");
-    //   var st=state.append("option");
-    //   st.text(ust[i]);
-    // }
-
-
-
-  //var stFiladata=filteredData.filter(filteredData => filteredData.state ===)
-    
-  
-    // remove any children from the table to
-    tbody.html("");
-    filteredData.forEach((signting) => {
-      var row = tbody.append("tr");
-      Object.entries(signting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    });
-  };
-
-   function runDate() {
+function runDate() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
     
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    var date = d3.select("#datetime");
   
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    var dateValue = date.property("value");
   
-    console.log(inputValue);
+    console.log(dateValue);
   
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-    var dupstates=filteredData.map(entry=>entry.state);
-    var ust=[...new Set(dupstates)];
-    console.log(ust[2]);
-    
+    var filteredData = tableData.filter(tableData => tableData.datetime === dateValue);
     console.log(filteredData);
+    
+    var dupCountry=filteredData.map(entry=>entry.country);
+    var uniqueCountry=[...new Set(dupCountry)];
+    console.log(uniqueCountry);
+    
+    var countryhtml=d3.select("#country");
+    for (var i=0;i<uniqueCountry.length; i++){
+      //var selstate=d3.select("#state");
+      var country=countryhtml.append("option");
+      country.text(uniqueCountry[i]);
+      
+    }
 
-    var select = d3.select('body')
-  .append('select')
-  	.attr('class','select')
-    .on('change',onchange)
-
-var options = select
-  .selectAll('option')
-	.data(data).enter()
-	.append('option')
-		.text(function (d) { return d; });
-
-function onchange() {
-	selectValue = d3.select('select').property('value')
-	d3.select('body')
-		.append('p')
-		.text(selectValue + ' is the last selected option.')
+    
 };
-    //var state=d3.select("#state");
-    // for (var i=0;i<ust.length; i++){
-    //   //var selstate=d3.select("#state");
-    //   // var st=state.append("option");
-    //   // st.text(ust[i]);
 
-    //   var st=state.append(attr('class','style'));
-    // var options = st.selectAll('option').append('option')
-    //         .text(ust[i]);//.forEach(function (d) {return d.Name}));
-    // console.log(options);
-    // }
+ function runCountry(){
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+    
+  // Select the input element and get the raw HTML node
+  var date = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var dateValue = date.property("value");
+
+  console.log(dateValue);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === dateValue);
+  console.log(filteredData);
+
+
+
+
+  var country=d3.select("#country");
+  var selectedcountry=country.property("value");
+  console.log(selectedcountry);
+
+
+  var filteredCountry=filteredData.filter(filteredData => filteredData.country === selectedcountry );
+  console.log(filteredCountry);
+
+  var dupStates = filteredCountry.map(entry => entry.state);
+  var uniqueState=[...new Set(dupStates)];
+  console.log(uniqueState);
+  var statehtml=d3.select("#state");
+  for (var i=0;i<uniqueState.length; i++){
+      //var selstate=d3.select("#state");
+    var state=statehtml.append("option");
+    state.text(uniqueState[i]);
   }
 
-  function runState(){
-    d3.event.preventDefault();
+  // tbody.html("");
+  // filteredData.forEach((signting) => {
+  // var row = tbody.append("tr");
+  // Object.entries(signting).forEach(([key, value]) => {
+  //   var cell = row.append("td");
+  //   cell.text(value);
+  //   });
+  // });
+ };
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-  
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
 
-    var state=d3.select("#state");
-    var selectedstate=state.select("option").value;
-    console.log(selectedstate);
 
-  }
+
+
+// Complete the event handler function for the form
+function runEnter() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+    
+  // Select the input element and get the raw HTML node
+  var date = d3.select("#datetime");
+
+  //var filDate=tableData.filter(tableData => tableData.date === inputDate);
+
+  // Get the value property of the input element
+  var dateValue = date.property("value");
+
+  var country=d3.select("#country");
+  var selectedcountry=country.property("value");
+
+
+
+
+
+
+  var state=d3.select("#state");
+  var selectedstate=state.property("value");
+  console.log(selectedstate);
+  var filteredD=tableData.filter(tableData => tableData.datetime === dateValue && tableData.country === selectedcountry && tableData.state === selectedstate );
+  console.log(filteredD);
+  var tbody=d3.select("tbody");
+  tbody.html("");
+  filteredD.forEach((signting) => {
+  var row = tbody.append("tr");
+  Object.entries(signting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+  });
+
+};
